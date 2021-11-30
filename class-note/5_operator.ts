d29e835667adaaff69576937778943c450f21257
K-J-HYEON <kjh950601@naver.com>
2021-11-30 20:17:04 +0900

    ts-playground

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,11 +1,11 @@
 // function logMessage(value: any) {
-//     console.log(value)
+//     console.log(value);
 // }
 // logMessage('hello');
 // logMessage(100);
 
-
 let seho: string | number | boolean;
+
 function logMessage(value: string | number) {
     if (typeof value === 'number') {
         value.toLocaleString();
@@ -13,11 +13,10 @@ function logMessage(value: string | number) {
     if (typeof value === 'string') {
         value.toString();
     }
-    throw new TypeError('value must be string or number')
+    throw new TypeError('value must be string or number');
 }
 logMessage('hello');
 logMessage(100);
-
 
 interface Developer {
     name: string;
@@ -29,25 +28,23 @@ interface Person {
     age: number;
 }
 
-function askSomeone(someone: Developer | Person) {
-    // someone.name;
-    // someone.skill
-    // someone.age
-}
-askSomeone({ name: '디벨로퍼', skill: '웹 개발'});
-askSomeone({ name: '캡틴', age: 27});
-    
-
-
-// function askSomeone(someone: Developer & Person) {
-//     someone.name;
-//     someone.skill;
-//     someone.age;
+// function askSomeone(someone: Developer | Person) {
+// someone.name;
+// someone.skill
+// someone.age
 // }
-// askSomeone({ name: '디벨로퍼', skill: '웹 개발', age: 27});
-// askSomeone({ name: '캡틴', age: 27});
+// askSomeone({ name: '디벨로퍼', skill: '웹 개발' });
+// askSomeone({ name: '제이', age: 27 });
+
+function askSomeone(someone: Developer & Person) {
+    someone.name;
+    someone.skill
+    someone.age
+}
+askSomeone({ name: '디벨로퍼', skill: '웹 개발', age: 27 });
+// askSomeone({ name: '제이', age: 27 });
+
 
 
 // let jay: string | number | boolean;
-// let jaehyeon: string & number & boolean;
-
+// let capt: string | number | boolean;
