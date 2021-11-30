50f5895ed826f6e9071566482adcdd55526c7a49
K-J-HYEON <kjh950601@naver.com>
2021-11-30 19:47:19 +0900

    ts-playground

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -3,22 +3,24 @@
 //     age: number;
 // }
 
-
-// type은 어느 곳에나 다 쓸 수 있다.
 type Person = {
     name: string;
     age: number;
 }
 
-const jay: Person = {
+let seho: Person = {
     name: 'jay',
     age: 27
 }
 
-type MyString = string
-const str: MyString = 'hello';
+type MyString = string;
+let str: MyString = 'hello';
 
-type Todo = { id: string; title: string; done: boolean }
+type Todo = {
+    id: string; title: string; done: boolean
+};
+
+
 function getTodo(todo: Todo) {
 
 }
