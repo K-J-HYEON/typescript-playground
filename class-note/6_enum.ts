daf54e9123297098d0ab90806cf2257a83b23d1c
K-J-HYEON <kjh950601@naver.com>
2021-11-30 20:32:14 +0900

    ts-playground

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,12 +1,10 @@
 enum Shoes {
-    Nike =' 나이키',
+    Nike = '나이키',
     Adidas = '아디다스'
 }
 
 let myShoes = Shoes.Nike;
 console.log(myShoes); // '나이키'
-
-
 
 // 예제
 enum Answer {
@@ -14,14 +12,18 @@ enum Answer {
     No = 'N',
 }
 
-
+// 예제
 function askQuestion(answer: Answer) {
     if (answer === Answer.Yes) {
-        console.log('정답입니다')
+        console.log('정답입니다');
     }
     if (answer === Answer.No) {
-        console.log('오답입니다')
+        console.log('오답입니다');
     }
 }
+
 askQuestion(Answer.Yes);
-// askQuestion('Yes');
+askQuestion('Yes');
+// askQuestion('예스');
+// askQuestion('y');
+// askQuestion('yes');
