9ee0a30e540d4bd1fe880ac6880fcbb00e826b5f
K-J-HYEON <kjh950601@naver.com>
2021-12-03 17:39:59 +0900

    ts-playground

--- quiz/2_address-book/src/a.ts
+++ quiz/2_address-book/src/a.ts
@@ -9,7 +9,7 @@ function fetchItems(): Promise<string[]> {
     let items: string[] = ['a', 'b', 'c'];
     return new Promise(function (resolve) {
         resolve(items);
-    });
+    })
 }
 fetchItems();
 
